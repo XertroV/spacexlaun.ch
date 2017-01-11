f2613bb6cf8791cbe28efbf20926000b891403a4
Max Kaye <m@xk.io>
2017-01-12 03:18:25 +1100

    Add host

--- spacexlaunch.py
+++ spacexlaunch.py
@@ -60,7 +60,7 @@ def update_forever():
 
 
 update_thread = threading.Thread(target=update_forever)
-update_thread.start()
+#update_thread.start()
 
 
 @app.route("/")
@@ -69,7 +69,7 @@ def spacex_template():
 
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
     url_for('static', filename='countdown.min.js')
     url_for('static', filename='moment-countdown.min.js')
